7578c3d6ffe23bf8806ebe9cb63ff66fb5abfaa5
Justyna Rachowicz <mrowa44@gmail.com>
2018-02-21 13:40:48 +0100

    Add RatingControl class

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,14 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Justyna Rachowicz on 21.02.2018.
+//  Copyright © 2018 Justyna Rachowicz. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+
+}
